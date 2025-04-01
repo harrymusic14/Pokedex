84fbad75bc176f7c3e976bf33dc1338754c96bcb
harrymusic14 <172458702+harrymusic14@users.noreply.github.com>
2025-03-31 21:16:23 -0500

    ingregar

--- src/app/Home.jsx
+++ src/app/Home.jsx
@@ -1,14 +1,20 @@
 function Home () {
     return (
         <div className="w-full h-dvh flex justify-center items-center">
-            <div className="border border-red-400">
-                <h1>POKEDEX</h1>
-                <h2>¡Hola entrenador!</h2>
-                <p>Para poder comenazar, dame un nombre</p>
+            <div className="text-center">
+                <h1 className="text-5xl font-semibold mb-4">POKEDEX</h1>
+                <h2 className="text-xl font-semibold text-red-500 ">¡Hola entrenador!</h2>
+                <p className="mb-4">Para poder comenazar, dame un nombre</p>
 
                 <div>
-                    <input type="text" />
-                    <button>Comenzar</button>
+                    <input 
+                    type="text" 
+                    className="bg-white shadow py-1.5 px-3 outline-0"
+                    placeholder="tu nombre"
+                    />
+                    <button className="bg-red-500 text-white py-1.5 px-3 cursor-pointer hover:bg-red-600 transition-colors duration-300 ease-in-out">  
+                        Comenzar
+                    </button>
                 </div>
             </div>
             <div className="absolute left-0 bottom-0 bg-black w-full h-20 mt-auto">

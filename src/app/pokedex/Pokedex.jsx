d686779f9ebce3fe52273da6526d2b5aef3f670f
harrymusic14 <172458702+harrymusic14@users.noreply.github.com>
2025-04-02 23:43:15 -0500

    holi

--- src/app/pokedex/Pokedex.jsx
+++ src/app/pokedex/Pokedex.jsx
@@ -8,6 +8,8 @@ const baseUrl = "https://pokeapi.co/api/v2/pokemon"
 function Pokedex() {
     const [state] = useName()
     const [pokemons, setPokemons] = useState([])
+    const [filteredPokemons, setFilterdPokemons] = useState([])
+    const [siglePokemon, setSinglePokemon] = useState(null)
     const [search, setSearch] = useState('')
     
 
@@ -22,22 +24,30 @@ function Pokedex() {
     useEffect(() => {
         getPokemons()
     }, [])
+
+    useEffect(() => {
+        if (!search) {
+            setFilterdPokemons(pokemons)
+            setSinglePokemon(null)
+            return
+        }
+        setFilterdPokemons(
+            pokemons.filter(p=>p.name.toLowerCase().includes(search.toLowerCase()))
+        )
+    }, [search, pokemons])
     
     const searchPokemon = () => {
         axios.get(baseUrl + '/' + search.toLowerCase())
             .then(response => {
-                console.log(response.data)
+                setSinglePokemon(response.data)
             })
             .catch(error => console.error(error))
     }
-    
-    const filteredPokemons = pokemons.filter(p => p.name.toLowerCase().includes(search.toLowerCase()))
 
     return (
         <div>
             <div className="max-w-5xl mx-auto px-4">
                 <h2 className="mb-9"><span className="text-red-500 font-semibold">Bienvenido {state.name}</span>, aquí podras encontrar tu pokemón favorito</h2>
-                {/* Aqui va el le buscador y el filtro */}
                 <div className="mb-9    ">
                     <input 
                         type="text"
@@ -45,14 +55,24 @@ function Pokedex() {
                         volue={search}
                         onChange={(e)=>setSearch(e.target.value)} 
                         className="input"
-                    />
+                        />
                     <button className="btn" onClick={searchPokemon}>
                         Buscar
                     </button>
                 </div>
 
+                {/* Aqui va el pokemón */}
+                
+
+                {siglePokemon ? (
+                    <div>
+                        <h2>{siglePokemon.name}</h2>
+                        <img src={siglePokemon.sprites?.front_default} alt={siglePokemon.name} />
+                    </div>
+                ) : (
+                    <PokemonsList pokemons={filteredPokemons}/>
+                )}  
                 {/* Aqui va la lista de pokemones */}
-                <PokemonsList pokemons={filteredPokemons}/>
             </div>
         </div>
     )
